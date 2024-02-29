14559b796f9f162b1d61995f0122847a462d1db2
Yalcin <bilgekagan902@gmail.com>
2024-02-29 17:12:56 +0300

    while loop

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace whileLoop{
+    class Program{
+        public static void Main(string[] args){
+            // while
+            int sayi = int.Parse(Console.ReadLine()); 
+            int sayac = 1;
+            int toplam = 0;
+            while(sayac <=sayi){
+                toplam +=sayac;
+                sayac++;
+            }
+            Console.WriteLine("Sonuc : " + toplam/sayi);
+            char charecter = 'a';
+            while(charecter<= 'z'){
+                Console.WriteLine(charecter);
+                charecter++;
+            }
+            // foreach
+            string [] arabalar = {"BMV","Ford","Toyota","Nissan"};
+            foreach(var araba in arabalar){
+                Console.WriteLine(araba);
+            }
+        }
+        
+    }
+}
